1a05d19507c976127945c499cdc90565e384048e
sergiobollo <58012294+sergiobollo@users.noreply.github.com>
2020-11-14 17:06:17 -0300

    update

--- clinica/urls.py
+++ clinica/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = "clinica"
 urlpatterns = [
-    path("", views.index, name = "index")
+    path("", views.index, name = "index"),
+    path("turnos", views.turnos, name = "turnos"),
+    path("pacientes", views.pacientes, name = "pacientes"),
+    path("<int:paciente_id>", views.historial_medico, name = "historial_medico"),
+    
 ]
--- clinica/views.py
+++ clinica/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Medico, Paciente, Turno, HistorialMedico, Producto, Pedido
+from django.http import HttpResponseBadRequest, HttpResponseRedirect, Http404
 
 # Create your views here.
 
@@ -12,4 +13,27 @@ def index(request):
         "historiales_medicos": HistorialMedico.objects.all(),
         "productos": Producto.objects.all(),
         "pedidos": Pedido.objects.all()
+        })
+    
+def turnos(request):
+    return render(request,"clinica/turnos.html", {
+        "titulo": "Gestión de turnos",
+        "turnos": Turno.objects.all()
+        })
+    
+def pacientes(request):
+    return render(request,"clinica/pacientes.html", {
+        "titulo": "Listado de pacientes",
+        "pacientes": Paciente.objects.all(),
+        })
+    
+def historial_medico(request, paciente_id):
+    paciente = Paciente.objects.get(id=paciente_id)
+    try:
+        historial_medico = HistorialMedico.objects.filter(paciente=paciente_id)
+    except HistorialMedico.DoesNotExist:
+        raise Http404("No se encontroó paciente")
+    return render(request,"clinica/historial_medico.html", {
+        "historial_medico": historial_medico,
+        "paciente": paciente
         })
