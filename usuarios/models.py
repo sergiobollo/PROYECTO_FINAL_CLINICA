e26105519252d66965a188d8aedc638c8f5226ad
sergiobollo <58012294+sergiobollo@users.noreply.github.com>
2020-11-20 00:26:04 -0300

    update

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    is_secretaria = models.BooleanField(default=False)
+    is_medico = models.BooleanField(default=False)
+    is_vendedor = models.BooleanField(default=False)
+    is_taller = models.BooleanField(default=False)
+    is_gerencia = models.BooleanField(default=False)
+
+class Secretaria(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    
+class Medico(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    
+class Vendedor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    
+class Taller(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    
+class Gerencia(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
